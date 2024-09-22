20dfcbf045d810487b056a5494c918b176c05482
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-21 20:28:18 -0500

    feat: Add try-catch to handle exceptions

--- src/main/java/org/stackbuilder/utils/DateTimeParser.java
+++ src/main/java/org/stackbuilder/utils/DateTimeParser.java
@@ -1,17 +1,28 @@
 package org.stackbuilder.utils;
 
+import org.stackbuilder.exception.InvalidInputException;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateTimeParser {
     public static LocalDate parseDate(String date) {
-        // Parsing date according specific format
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        try {
+            // Parsing date according to specific format
+            return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidInputException("Invalid Format");
+        }
     }
 
     public static LocalTime parseTime(String time) {
-        // Parsing hour according specific format
-        return LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+        try {
+            // Parsing hour according to specific format
+            return LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidInputException("Invalid Format");
+        }
     }
 }
