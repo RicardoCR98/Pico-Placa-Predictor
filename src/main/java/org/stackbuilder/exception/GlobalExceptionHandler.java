1ad4fec5666bb808e19799278b9f08ad84306eaf
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-21 19:32:21 -0500

    feat: Add the exceptions: GlobalExceptionHandler.java  and InvalidInputException.java

---
+++ src/main/java/org/stackbuilder/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.stackbuilder.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<String> handleInvalidInput(InvalidInputException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
