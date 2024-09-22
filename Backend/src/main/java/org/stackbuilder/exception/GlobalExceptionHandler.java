87574ead79ce652a009d2978057248239963a005
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-22 00:45:39 -0500

    feat: Update exepctions message

--- Backend/src/main/java/org/stackbuilder/exception/GlobalExceptionHandler.java
+++ Backend/src/main/java/org/stackbuilder/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package org.stackbuilder.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Collections;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidInputException.class)
-    public ResponseEntity<String> handleInvalidInput(InvalidInputException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleInvalidInput(InvalidInputException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonMap("message", ex.getMessage()));
     }
 
 }
