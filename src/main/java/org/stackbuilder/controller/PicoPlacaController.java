5394d23831f6896ea898389b537d591b6d9c9a3f
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-21 19:31:23 -0500

    feat: Add the controller PicoPlacaController.java

---
+++ src/main/java/org/stackbuilder/controller/PicoPlacaController.java
@@ -0,0 +1,27 @@
+package org.stackbuilder.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.stackbuilder.model.PredictionRequest;
+import org.stackbuilder.model.PredictionResponse;
+import org.stackbuilder.service.PicoPlacaService;
+
+@RestController
+@RequestMapping("/api")
+public class PicoPlacaController {
+
+    private final PicoPlacaService picoPlacaService;
+
+    public PicoPlacaController(PicoPlacaService picoPlacaService) {
+        this.picoPlacaService = picoPlacaService;
+    }
+
+    @PostMapping("/predict")
+    public ResponseEntity<PredictionResponse> predict(@RequestBody PredictionRequest request) {
+        PredictionResponse response = picoPlacaService.canDrive(request);
+        return ResponseEntity.ok(response);
+    }
+}
