5f7b04936a58e3e4b3ac9eed841db6b88c323651
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-21 22:23:28 -0500

    Fix: Update .gitignore

--- src/main/java/org/stackbuilder/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package org.stackbuilder.exception;
-
-public class InvalidInputException extends RuntimeException{
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
