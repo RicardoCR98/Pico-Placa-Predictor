1bd2fb5c818c7dec4fa8afced445911f8744f7c1
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-22 00:45:49 -0500

    feat: Add cors

--- Backend/src/main/java/org/stackbuilder/Main.java
+++ Backend/src/main/java/org/stackbuilder/Main.java
@@ -2,10 +2,28 @@ package org.stackbuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                        .allowedMethods("POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
 }
