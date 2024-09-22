4136f4fec119badbeebd3e6b1ecf6cfbc8d9e239
RicardoCR98 <gary.campana@epn.edu.ec>
2024-09-21 19:32:56 -0500

    feat: Add the tests: PicoPlacaServiceTest.java and PicoPlacaControllerTest.java

---
+++ src/test/java/PicoPlacaServiceTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.stackbuilder.exception.InvalidInputException;
+import org.stackbuilder.model.PredictionRequest;
+import org.stackbuilder.model.PredictionResponse;
+import org.stackbuilder.service.PicoPlacaService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = PicoPlacaService.class)
+public class PicoPlacaServiceTest {
+    @Autowired
+    private PicoPlacaService picoPlacaService;
+    /* Tests:
+    * Vehicle restricted during prohibited hours and days.
+    * Vehicle not restricted outside of hours.
+    * Vehicle not restricted on weekends.
+    * Handling of invalid entries.
+    * */
+    @Test
+    public void testRestrictedDuringProhibitedHoursAndDays() {
+        PredictionRequest request = new PredictionRequest("ABC-1234","17-09-2024","07:00");
+        PredictionResponse response  = picoPlacaService.canDrive(request);
+        assertFalse(response.isCanDrive());
+    }
+
+    @Test
+    public void testNotRestrictedOutsideHours() {
+        PredictionRequest request = new PredictionRequest("ABC-1234","17-09-2024","10:00");
+        PredictionResponse response  = picoPlacaService.canDrive(request);
+        assertTrue(response.isCanDrive());
+    }
+
+    @Test
+    public void testNotRestrictedWeekends(){
+        PredictionRequest request = new PredictionRequest("ABC-1234","22-09-2024","08:00");
+        PredictionResponse response  = picoPlacaService.canDrive(request);
+        assertTrue(response.isCanDrive());
+    }
+
+    @Test
+    public void testInvalidIputs(){
+        PredictionRequest request = new PredictionRequest("AA-1234","17-09-2024","10:00");
+        InvalidInputException exception = assertThrows(InvalidInputException.class, () -> {
+            picoPlacaService.canDrive(request);
+        });
+        assertEquals("Invalid Format", exception.getMessage());
+    }
+}
